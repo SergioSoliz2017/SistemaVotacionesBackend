3b135b98060aa4b80024c2afb9ca4d3ab6669aab
SergioSoliz2017 <201801656@est.umss.edu>
2023-10-16 18:59:27 -0400

    UnionTodo

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',function(){
+    return "holamundo";
 });
+
+Route::get('/verificarAdministrador/{id}',[App\Http\Controllers\AdministradorController::class,"verificarAdministrador"]);
+Route::get('/obtenerProcesosElectorales',[App\Http\Controllers\ProcesoElectoralController::class,"obtenerProcesosElectorales"]);
+
+Route::post('/crearProcesoElectoral',[App\Http\Controllers\ProcesoElectoralController::class,"agregarProcesoElectoral"]);
+
+
+Route::post('/asignar-vocales/{COD_COMITE}', [App\Http\Controllers\PoblacionController::class, 'asignarVocales']);
+
+Route::get('/elecciones', [App\Http\Controllers\EleccionController::class, 'index']);
+
+Route::put('/asignar-comite/{COD_ELECCION}', [App\Http\Controllers\ComiteElectoralController::class, 'asignarComite']);
+
+
+Route::get('/ver-lista-comite/{idComite}', [App\Http\Controllers\AsociarTitularSuplenteController::class, 'verListaComite']);
+
